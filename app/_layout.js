a9f03fffe1ca72711574a5f6964d9b2b4761107b
Kapil619 <kapilbadokar321@gmail.com>
2024-10-01 20:00:16 +0530

    Added expo-font package, Configured Routing, Disbaled API Keys, Implementing expo-router .

--- app/_layout.js
+++ app/_layout.js
@@ -1,24 +1,21 @@
-import { useNavigation } from "@react-navigation/native";
-// import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { collection, doc, getDoc } from "firebase/firestore";
 import { useEffect } from "react";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, Alert, View } from "react-native";
 import { db } from "./firebase";
+import { useFonts } from "expo-font";
 
 const Layout = () => {
-  // const [fontsLoaded] = useFonts({
-  //   DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
-  //   DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
-  //   DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
-  // });
-
-  const navigation = useNavigation();
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+  const router = useRouter();
 
   useEffect(() => {
     const auth = getAuth();
-
     // Listen for auth state changes
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
@@ -27,25 +24,53 @@ const Layout = () => {
         );
         const userData = userDoc.data();
         if (userData && userData.role === "employer") {
-          navigation.navigate("employer/employerHome");
+          Alert.alert("You are an employer");
+          router.push("employer/employerHome");
         } else {
-          navigation.navigate("home");
+          Alert.alert("You are a job seeker");
+          router.push("home");
         }
       } else {
-        navigation.navigate("auth");
+        Alert.alert("You are not signed in");
+        router.push("auth");
       }
     });
-    // Cleanup subscription on unmount
     return () => unsubscribe();
-  }, [navigation]);
+  }, [router]);
 
-  // if (!fontsLoaded) {
-  //   return <ActivityIndicator size="large" color={"#fff"} />;
-  // }
-
+  if (!fontsLoaded) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          marginVertical: "50%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size="large" color={"green"} />
+      </View>
+    );
+  }
   return (
     <Stack>
       <Stack.Screen options={{ header: () => null }} name="auth" />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="home" options={{ headerShown: true }} />
+      <Stack.Screen name="about" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="employer/createJobs"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="employer/[id]" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="employer/EmployerAuth"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="employer/employerProfile"
+        options={{ headerShown: false }}
+      />
     </Stack>
   );
 };
--- app/index.js
+++ app/index.js
@@ -1,5 +1,19 @@
-import { Redirect } from "expo-router";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-export default function Index() {
-  return <Redirect href="/auth" />;
+export default function Loading() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        marginVertical: "50%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ActivityIndicator size="large" color={"red"} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({});
