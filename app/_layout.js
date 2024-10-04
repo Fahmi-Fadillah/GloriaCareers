e9d179d5abd09fde6cef1e7e6f5778ce222c943e
Kapil619 <kapilbadokar321@gmail.com>
2024-10-04 19:36:23 +0530

    Configured expo-updates & eas updates within the app.

--- app/_layout.js
+++ app/_layout.js
@@ -7,13 +7,31 @@ import { ActivityIndicator, View } from "react-native";
 import { db } from "./firebase";
 import { RootSiblingParent } from "react-native-root-siblings";
 import JobApplicants from "./employer/jobApplicants";
+import * as Updates from "expo-updates";
 
 const Layout = () => {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
   const [fontsLoaded] = useFonts({
     DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
     DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
     DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
   });
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
   const router = useRouter();
   // const [isMounted, setIsMounted] = useState(false);
   // useEffect(() => {
